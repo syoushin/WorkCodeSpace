77da0e499df6cecd3b56c7597baf847853c519f8
syoushin <john_zhx@outlook.com>
2015-12-10 10:35:53 +0800

    first commite

---
+++ JohnCheckPoint/ApplicationTriggerTask.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace JohnCheckPoint
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ApplicationTriggerTask : Page
+    {
+        // A pointer back to the main page.  This is needed if you want to call methods in MainPage such
+        // as NotifyUser()
+        MainPage rootPage = MainPage.Current;
+
+        // A pointer to the ApplicationTrigger so we can signal it later
+        ApplicationTrigger trigger = null;
+
+        public ApplicationTriggerTask()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == BackgroundTaskSample.ApplicationTriggerTaskName)
+                {
+                    AttachProgressAndCompletedHandlers(task.Value);
+                    BackgroundTaskSample.UpdateBackgroundTaskStatus(BackgroundTaskSample.ApplicationTriggerTaskName, true);
+                    break;
+                }
+            }
+
+            trigger = new ApplicationTrigger();
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Attach progress and completed handers to a background task.
+        /// </summary>
+        /// <param name="task">The task to attach progress and completed handlers to.</param>
+        private void AttachProgressAndCompletedHandlers(IBackgroundTaskRegistration task)
+        {
+            task.Progress += new BackgroundTaskProgressEventHandler(OnProgress);
+            task.Completed += new BackgroundTaskCompletedEventHandler(OnCompleted);
+        }
+
+        /// <summary>
+        /// Handle background task completion.
+        /// </summary>
+        /// <param name="task">The task that is reporting completion.</param>
+        /// <param name="e">Arguments of the completion report.</param>
+        private void OnCompleted(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Handle background task progress.
+        /// </summary>
+        /// <param name="task">The task that is reporting progress.</param>
+        /// <param name="e">Arguments of the progress report.</param>
+        private void OnProgress(BackgroundTaskRegistration sender, BackgroundTaskProgressEventArgs args)
+        {
+            var progress = "Progress: " + args.Progress + "%";
+            BackgroundTaskSample.ApplicationTriggerTaskProgress = progress;
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Register a ApplicationTriggerTask.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void RegisterBackgroundTask(object sender, RoutedEventArgs e)
+        {
+            var task = BackgroundTaskSample.RegisterBackgroundTask(BackgroundTaskSample.SampleBackgroundTaskEntryPoint,
+                                                                   BackgroundTaskSample.ApplicationTriggerTaskName,
+                                                                   trigger,
+                                                                   null);
+            await task;
+            AttachProgressAndCompletedHandlers(task.Result);
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Unregister a ApplicationTriggerTask.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UnregisterBackgroundTask(object sender, RoutedEventArgs e)
+        {
+            BackgroundTaskSample.UnregisterBackgroundTasks(BackgroundTaskSample.ApplicationTriggerTaskName);
+            BackgroundTaskSample.ApplicationTriggerTaskResult = "";
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Signal a ApplicationTriggerTask.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void SignalBackgroundTask(object sender, RoutedEventArgs e)
+        {
+            // Reset the completion status
+            var settings = ApplicationData.Current.LocalSettings;
+            settings.Values.Remove(BackgroundTaskSample.ApplicationTriggerTaskName);
+
+            //Signal the ApplicationTrigger
+            var result = await trigger.RequestAsync();
+            BackgroundTaskSample.ApplicationTriggerTaskResult = "Signal result: " + result.ToString();
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Update the scenario UI.
+        /// </summary>
+        private async void UpdateUI()
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                RegisterButton.IsEnabled = !BackgroundTaskSample.ApplicationTriggerTaskRegistered;
+                UnregisterButton.IsEnabled = BackgroundTaskSample.ApplicationTriggerTaskRegistered;
+                SignalButton.IsEnabled = BackgroundTaskSample.ApplicationTriggerTaskRegistered & (trigger != null);
+                Progress.Text = BackgroundTaskSample.ApplicationTriggerTaskProgress;
+                Result.Text = BackgroundTaskSample.ApplicationTriggerTaskResult;
+                Status.Text = BackgroundTaskSample.GetBackgroundTaskStatus(BackgroundTaskSample.ApplicationTriggerTaskName);
+            });
+        }
+    }
+}
---
+++ JohnCheckPoint/SampleBackgroundTask.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace JohnCheckPoint
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SampleBackgroundTask : Page
+    {
+        public SampleBackgroundTask()
+        {
+            this.InitializeComponent();
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == BackgroundTaskSample.SampleBackgroundTaskName)
+                {
+                    AttachProgressAndCompletedHandlers(task.Value);
+                    BackgroundTaskSample.UpdateBackgroundTaskStatus(BackgroundTaskSample.SampleBackgroundTaskName, true);
+                    break;
+                }
+            }
+
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Update the scenario UI.
+        /// </summary>
+        private async void UpdateUI()
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+           () =>
+           {
+               RegisterButton.IsEnabled = !BackgroundTaskSample.SampleBackgroundTaskRegistered;
+               UnregisterButton.IsEnabled = BackgroundTaskSample.SampleBackgroundTaskRegistered;
+               Progress.Text = BackgroundTaskSample.SampleBackgroundTaskProgress;
+               Status.Text = BackgroundTaskSample.GetBackgroundTaskStatus(BackgroundTaskSample.SampleBackgroundTaskName);
+           });
+        }
+
+
+        /// <summary>
+        /// Attach progress and completed handers to a background task.
+        /// </summary>
+        /// <param name="task">The task to attach progress and completed handlers to.</param>
+        private void AttachProgressAndCompletedHandlers(IBackgroundTaskRegistration task)
+        {
+            task.Progress += new BackgroundTaskProgressEventHandler(OnProgress);
+            task.Completed += new BackgroundTaskCompletedEventHandler(OnCompleted);
+        }
+
+
+        /// <summary>
+        /// Handle background task completion.
+        /// </summary>
+        /// <param name="task">The task that is reporting completion.</param>
+        /// <param name="e">Arguments of the completion report.</param>
+        private void OnCompleted(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            UpdateUI();
+        }
+
+
+        /// <summary>
+        /// Handle background task progress.
+        /// </summary>
+        /// <param name="task">The task that is reporting progress.</param>
+        /// <param name="e">Arguments of the progress report.</param>
+        private void OnProgress(BackgroundTaskRegistration sender, BackgroundTaskProgressEventArgs args)
+        {
+            var progress = "Progress: " + args.Progress + "%";
+            BackgroundTaskSample.SampleBackgroundTaskProgress = progress;
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Unregister a SampleBackgroundTask.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void RegisterBackgroundTask(object sender, RoutedEventArgs e)
+        {
+            var task = BackgroundTaskSample.RegisterBackgroundTask(BackgroundTaskSample.SampleBackgroundTaskEntryPoint,
+                                                                   BackgroundTaskSample.SampleBackgroundTaskName,
+                                                                   new SystemTrigger(SystemTriggerType.TimeZoneChange, false),
+                                                                   null);
+            await task;
+            AttachProgressAndCompletedHandlers(task.Result);
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Unregister a SampleBackgroundTask.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UnregisterBackgroundTask(object sender, RoutedEventArgs e)
+        {
+            BackgroundTaskSample.UnregisterBackgroundTasks(BackgroundTaskSample.SampleBackgroundTaskName);
+            UpdateUI();
+        }
+    }
+}
